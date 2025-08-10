055a6888792f6264961ba8cb515309ad7192149b
Charlie <charlie@Charlies-Mac-mini.local>
2025-08-10 16:05:13 -0700

    New Contract Create button added

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext', // ensures dynamic imports work
+  },
+  base: '/', // or your correct subpath
   plugins: [react()],
 })
